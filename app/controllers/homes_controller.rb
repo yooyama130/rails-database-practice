5234c17238e4f65a2380d52f4cbbc520637cceaf
yooyama130 <yooyama.97.01@gmail.com>
2022-03-27 05:45:38 +0000

    [Update] ジャンルで絞り込みできるように

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,13 @@
 class HomesController < ApplicationController
   def top
-    @products = Product.all
+    # 絞り込みがあれば、その内容で絞り込む
+    if params[:genre].present?
+      genre_keyword = params[:genre]
+      @products = Product.where(genre: genre_keyword)
+    else
+      @products = Product.all
+    end
+    # uniqは、配列の重複している要素を消す
+    @all_genre = Product.all.pluck(:genre).uniq
   end
 end
