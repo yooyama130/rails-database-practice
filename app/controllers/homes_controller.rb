d0504b661511ebf1622890159b4a2bd05e8e9aa9
yooyama130 <yooyama.97.01@gmail.com>
2022-03-28 08:09:50 +0000

    [Update] ジャンル絞り込み時、横にSQL文を載せるように

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,12 @@ class HomesController < ApplicationController
   def top
     # 絞り込みがあれば、その内容で絞り込む
     if params[:genre].present?
-      genre_keyword = params[:genre]
-      @products = Product.where(genre: genre_keyword)
+      # SQL文表示に使う------------------
+      @sql = "sort_genre"
+      @genre_keyword = params[:genre]
+      @table_name = Product.table_name
+      # SQL文表示に使う　ここまで------------------
+      @products = Product.where(genre: @genre_keyword)
     else
       @products = Product.all
     end
